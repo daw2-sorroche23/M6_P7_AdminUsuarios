7dc6c0060e9835d62a6cc5d7a1b36b25cb17e1c5
daw2-gil23 <gilalvarezmaria@fpllefia.com>
2023-02-03 14:56:09 +0100

    Inicial

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from 'path';
+import path from "path";
+export default {
+    root: path.resolve(__dirname, "src"),
+    build: {
+        outDir: path.resolve(__dirname, "dist"),
+        rollupOptions: {
+            input: {
+                index: path.resolve(__dirname, "src/index.html"),
+               
+            },
+        },
+    },
+    base: './'
+};
